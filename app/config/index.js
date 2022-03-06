84661462cc858fce2cad8fb4a1d2de49561f5837
Thien Pham Van <PVThien21>
2022-03-06 19:29:08 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -2,9 +2,9 @@ const config  = {
     app: {
         port: process.env.PORT || 8080,
     },
-    // db: {
-    //     uri: process.env.MONGODB_URI || "mongodb://localhost:27017/contactbook"
-    // }
+    db: {
+        uri: process.env.MONGODB_URI || "mongodb://localhost:27017/contactbook"
+    }
 }
 
 module.exports = config;
---
+++ app/models/contact.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+    {
+        name: {
+            type: String, 
+            required: [true, "Contact name is required"],
+        },
+        email: {
+            type: String,
+            trim: true, 
+            Lowercase: true,
+        },
+        address: String,
+        phone: String, 
+        favorite: Boolean,
+    },
+    { timestamps: true }
+);
+
+// Replace_id with id and remoce --V
+schema.method("toJSON", function(){
+    const { __v, _id, ...object} = this.toObject();
+    object.id = _id;
+    return object;
+});
+
+module.exports = mongoose.model("contact", schema);
+
+
+
+
